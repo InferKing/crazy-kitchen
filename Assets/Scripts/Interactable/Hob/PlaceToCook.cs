97b86f3282a81ad16c63d243188f2893f7aa74e5
1nfer.exe <InferKing@users.noreply.github.com>
2024-04-09 11:44:35 +0500

    add some shit that anyone can't

--- Assets/Scripts/Interactable/Hob/PlaceToCook.cs
+++ Assets/Scripts/Interactable/Hob/PlaceToCook.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class PlaceToCook : Interactable
 {
+    [SerializeField] private LayerMask _interactableMask;
     [SerializeField] private HobToggle _toggle;
+    [SerializeField] private float _maxDistance = 0.2f;
     private BoxCollider _collider;
     private Dishes _dish;
     public bool IsEmpty { get; private set; } = true;
@@ -14,6 +16,21 @@ public class PlaceToCook : Interactable
     {
         base.Start();
         _collider = GetComponent<BoxCollider>();
+    }
+    protected virtual void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new(transform.position, Vector3.up);
+#if UNITY_EDITOR
+        Debug.DrawRay(ray.origin, ray.direction * _maxDistance, Color.red);
+#endif
+        if (Physics.Raycast(ray, out hit, _maxDistance, _interactableMask))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+#if UNITY_EDITOR
+            Debug.DrawRay(ray.origin, ray.direction * _maxDistance, Color.green);
+#endif
+        }
     }
     public override bool TryCombine(Interactable interactable, out bool stayInHand)
     {
@@ -26,7 +43,6 @@ public class PlaceToCook : Interactable
             // 1 - каждый раз при интеракции со сковородкой или другим предметом уведомлять об этом всех!!!! (хуита)
             // 2 - переделать плиту так, чтобы она сама проверяла, есть ли сейчас над комфоркой посуда или нет (тоже хуита, 4 комфорки = 4 рэйкаста)
             // 3 - то же, что и вариант 2, но проверять триггерами (хз пока)
-            Debug.Log("here");
             ToStove((Dishes)interactable);
             // GameObject new_mesh = GetGameObject();
             // ServiceLocator.Instance.Get<EventBus>().Invoke(new DestroyMeDaddySignal(interactable.gameObject));
@@ -41,7 +57,7 @@ public class PlaceToCook : Interactable
         _dish.transform.SetParent(transform);
         _dish.transform.eulerAngles = _dish.InitRotation;
         _dish.Rb.isKinematic = true;
-        _dish.transform.position = transform.position;
+        _dish.transform.position = new Vector3(transform.position.x, transform.position.y + 0.03f, transform.position.z);
         _collider.enabled = false;
     }
 
