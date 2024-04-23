07158dac080b549dfcb1fc9c4b33938bf3fef5ba
Oleg <54029296+InferKing@users.noreply.github.com>
2024-04-23 18:46:23 +0500

    add fx smoke

--- Assets/Scripts/Interactable/Hob/Hob.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Hob : MonoBehaviour
-{
-    [SerializeField] private List<GameObject> _placesToCook;
-    //[SerializeField] private List<>
-}
