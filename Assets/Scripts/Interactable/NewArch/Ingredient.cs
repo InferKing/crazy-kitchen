d2c3cdbb8a62ba2b62d6c843916a4d6697e83243
Oleg <54029296+InferKing@users.noreply.github.com>
2024-05-03 20:25:52 +0500

    Update Ingredient.cs

--- Assets/Scripts/Interactable/NewArch/Ingredient.cs
+++ Assets/Scripts/Interactable/NewArch/Ingredient.cs
@@ -1,6 +1,3 @@
-using UnityEngine;
-using System.Collections.Generic;
-
 public class Ingredient : Grabbable
 {
     public override void OnEnter()
@@ -14,25 +11,4 @@ public class Ingredient : Grabbable
         base.OnExit();
         Bus.Invoke(new ShowItemTextSignal(string.Empty));
     }
-
-    //public override bool TryCombine(Interactable interactable, out bool stayInHand)
-    //{
-    //    stayInHand = false;
-    //    if (interactable == null) return false;
-    //    if (interactable is Dishes)
-    //    {
-    //        Dishes dish = interactable as Dishes;
-    //        dish.AddIngredient(this);
-    //        dish.PlaceIngredient(this);
-    //        stayInHand = true;
-    //        return true;
-    //    }
-    //    if (interactable is Knife)
-    //    {
-    //        stayInHand = true;
-    //        //GameObject new_mesh = GetGameObject();
-    //        return true;
-    //    }
-    //    return false;
-    //}
 }
